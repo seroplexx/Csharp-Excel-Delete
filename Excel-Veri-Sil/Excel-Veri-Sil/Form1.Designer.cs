c89639becf8e712de5e0990ad999eac95aae9d9b
Menderes Musa USLAN <menderes_855@outlook.com>
2022-04-23 13:18:07 +0300

    Excel Veri Silme
    Excel int. kütüphanesi ile veri silme

---
+++ Excel-Veri-Sil/Excel-Veri-Sil/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Excel_Veri_Sil
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Excel Veri SİL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 218);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(385, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(79, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Silinecek Satır ID";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(409, 277);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Excel-Veri-Sil/Excel-Veri-Sil/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Drawing.Printing;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace Excel_Veri_Sil
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dosya_yolu = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Stok\\----dosyaadı----;Extended Properties='Excel 8.0;HDR=YES';";
+        string dosya_yolu2 = "C:\\Stok\\dosyaadı.xlsx";
+        private void delete_fonk(int sayfa, string s2)
+        {
+            
+            OleDbConnection conn_up = new OleDbConnection(dosya_yolu);
+            conn_up.Open();
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter("SELECT * FROM [Sayfa" + s2 + "$] ", conn_up);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            conn_up.Close();
+
+            GC.GetTotalMemory(false);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.GetTotalMemory(true);
+
+
+            //System.Threading.Thread.Sleep(3000);
+
+            String expression = dt.Columns[0].ToString() + " = '" + textBox1.Text + "'";
+
+            DataRow[] dr = dt.Select(expression);
+            if (0 != dr.Length)
+            {
+                Excel.Application ExcelApp = new Excel.Application();
+                //System.Threading.Thread.Sleep(3000);
+
+                ExcelApp.Visible = false;
+
+                Excel.Workbook ExcelWorkbook = ExcelApp.Workbooks.Open(dosya_yolu2);
+                Excel.Worksheet ExcelWorksheet = ExcelWorkbook.Sheets[sayfa];
+                //System.Threading.Thread.Sleep(3000);
+
+                for (int index = 0; index < dr.Length; index++)
+                {
+                    string toDelete = "A" + (dt.Rows.IndexOf(dr[index]) + 2).ToString();
+                    Excel.Range cells = (Excel.Range)ExcelWorksheet.Range[toDelete, Type.Missing];
+                    cells.EntireRow.Delete();
+
+                }
+
+                ExcelWorkbook.Save();
+                ExcelWorkbook.Close();
+                ExcelApp.Quit();
+                ExcelApp.Quit();
+
+                Marshal.ReleaseComObject(ExcelWorksheet);
+                Marshal.ReleaseComObject(ExcelWorksheet);
+
+
+                Marshal.ReleaseComObject(ExcelWorkbook);
+                Marshal.ReleaseComObject(ExcelWorkbook);
+                Marshal.ReleaseComObject(ExcelApp);
+                Marshal.ReleaseComObject(ExcelApp);
+
+
+                ExcelWorksheet = null;
+                ExcelWorksheet = null;
+
+                ExcelWorkbook = null;
+                ExcelWorkbook = null;
+                ExcelApp = null;
+
+                GC.GetTotalMemory(false);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.GetTotalMemory(true);
+
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            progressBar1.Value = 25;
+            delete_fonk(1, "");
+            GC.GetTotalMemory(false);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.GetTotalMemory(true);
+            progressBar1.Value = 50;
+
+            delete_fonk(2, "1");
+            GC.GetTotalMemory(false);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.GetTotalMemory(true);
+            progressBar1.Value = 75;
+            delete_fonk(3, "2");
+            GC.GetTotalMemory(false);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.GetTotalMemory(true);
+
+
+            progressBar1.Value = 100;
+            System.Threading.Thread.Sleep(2000);
+            MessageBox.Show("" + textBox1.Text + " Numaralı Ürün Silinmiştir.", "Bilgi Penceresi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBar1.Visible = false;
+
+            textBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
